3c9ffca368a8bd27b6b79c0a89149ccbb2436592
kaloian3141 <kaloian70308@gmail.com>
2026-02-19 23:06:01 +0200

    Initial backend Spring Boot structure

---
+++ backend/src/test/java/com/Edu_App/EduAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Edu_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
