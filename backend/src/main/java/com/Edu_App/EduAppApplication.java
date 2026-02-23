19d16275ee9d4a855f6091beb0cbc1f694395b47
Victoruncho <viktor.g.hristov.2022@elsys-bg.org>
2026-02-23 17:12:21 +0200

    Finalized version of pom.xml, application.properties; finalized login controller + login request class + postgreSQL test

--- backend/src/main/java/com/Edu_App/EduAppApplication.java
+++ backend/src/main/java/com/Edu_App/EduAppApplication.java
@@ -3,6 +3,12 @@ package com.Edu_App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import com.Edu_App.domain.entities.UserEntity;
+import com.Edu_App.repositories.UserRepository;
+
 @SpringBootApplication
 public class EduAppApplication {
 
@@ -10,4 +16,20 @@ public class EduAppApplication {
 		SpringApplication.run(EduAppApplication.class, args);
 	}
 
+	@Bean
+CommandLineRunner run(UserRepository userRepository) {
+    return args -> {
+        UserEntity testUser = UserEntity.builder()
+                .username("Ivan_Ivanov")
+                .email("ivan@example.com")
+                .hashPassword("secret123")
+                .build();
+
+        userRepository.save(testUser);
+        System.out.println("save a rec to pgAdmin.");
+    };
+
 }
+}
+
+
