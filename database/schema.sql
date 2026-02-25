28cd80239804794475ff81bbd0a8bdd290972837
IgnatStoychev <ignat.v.stoychev.2022@elsys-bg.org>
2026-02-25 14:42:45 +0200

    Ignat commit 3, dtabase3

--- database/schema.sql
+++ database/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE currencies (
     id SERIAL PRIMARY KEY,
     code CHAR(3) UNIQUE NOT NULL,  
     name TEXT NOT NULL 
-
+);
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
@@ -16,3 +16,18 @@ CREATE TABLE users (
 );
 
 
+CREATE TABLE accounts (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    currency TEXT NOT NULL,
+    balance DECIMAL(15,2) DEFAULT 0
+);
+
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    from_account_id INT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    to_account_id INT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    amount DECIMAL(15,2) NOT NULL,
+    timestamp TIMESTAMP DEFAULT now()
+);
