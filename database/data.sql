89b0ba117c6fe9c03c7206773ea77958a588a2d3
IgnatStoychev <ignat.v.stoychev.2022@elsys-bg.org>
2026-02-27 15:10:43 +0200

    Ignat commits,shema.sql + data.sql

---
+++ database/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO currencies (code, exchange_rate) VALUES
+('BGN', 1.0000),
+('EUR', 1.9558),
+('USD', 1.8000),
+('GBP', 2.3000);
--- database/schema.sql
+++ database/schema.sql
@@ -6,29 +6,28 @@ DROP TABLE IF EXISTS currencies;
 
 CREATE TABLE currencies (
     id SERIAL PRIMARY KEY,
-    code CHAR(3) UNIQUE NOT NULL,  
-    name TEXT NOT NULL 
+    code CHAR(3) UNIQUE NOT NULL,
+    exchnage_rate DECIMAL(10,4) NOT NULL
 );
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT now()
 );
-
-
 CREATE TABLE accounts (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    currency TEXT NOT NULL,
+    currency_id INT NOT NULL REFERENCES currencies(id),
+    iban TEXT UNIQUE NOT NULL,
     balance DECIMAL(15,2) DEFAULT 0
 );
-
-
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
     from_account_id INT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
     to_account_id INT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    currency_id INT NOT NULL REFERENCES currencies(id),
     amount DECIMAL(15,2) NOT NULL,
     timestamp TIMESTAMP DEFAULT now()
 );
